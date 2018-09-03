7e91ac1ecc8f326ce60c6d3b49b91d3349faa9ae
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-03 13:10:30 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
-def greeting(name ="Sally")
-  phrase = "Hello ${name}. It's nice to meet you."
+def greeting(name)
+  phrase = "Hello #{name}. It's nice to meet you."
 end
